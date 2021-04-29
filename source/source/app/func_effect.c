76b0a44fa3ab5c98166e694a29c4dfc9a66561ad
Yoshihiro Tsurui <tsuruiy@gmail.com>
2021-04-29 18:57:24 +0900

    室内灯エフェクト(電球)の追加

--- source/source/app/func_effect.c
+++ source/source/app/func_effect.c
@@ -101,7 +101,30 @@ uint8_t funcEffect(uint8_t stat, uint8_t cvType, uint8_t counter, uint16_t *effe
 			return (iValue2);
 			
 		}
-	}
+	} else if ((cvType & 0x0F) == 0x06) {
+		// Roomlight Type3
+		if (stat == 0) {
+			*effectCounter = 0;
+			*HSfuncValue = 0;
+			*ctrlOverride = 0;
+			return (0);
+		}
+		if (*effectCounter != 0) {
+			iValue2 = (*effectCounter >> 2);
+			if ((iValue2 >> 4) >= iValue) {
+				*effectCounter = 0;
+				*HSfuncValue = (iValue2 & 0xF0) + 0x0F;
+				*ctrlOverride = 1;
+				return (iValue);
+			}
+			*HSfuncValue = iValue2;
+			*ctrlOverride = 1;
+			return (iValue2);
+		} else {
+			*HSfuncValue = (cvType & 0xF0) + 0x0F;
+			*ctrlOverride = 1;
+		}
+	} 
 	
 	
 	
